63122b6623f6ad605c66649198d4307cb3ecfa09
Vinurlan <dosug.d@gmail.com>
2020-01-31 02:30:53 +0300

    getStarted

---
+++ src/components/ReqestForm.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="form-box">
+        <form class="form-request">
+            <input type="text" placeholder="Name">
+            <input type="number" placeholder="Phone">
+            <input type="text" placeholder="Comment"> 
+            <button @click.prevent>Отправить</button>
+            <button @click.prevent="close">Закрыть</button>
+        </form>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: "ReqestForm",
+    props: {
+        close: Function
+    }
+}
+
+</script>
+
+<style scoped>
+.form-request {
+    position: fixed;
+    top: 40%;
+    left: 38%;
+    background-color: #fff;
+    border: 1px solid #000;
+    width: 200px;
+    height: 200px;
+}
+
+input {
+    margin: 10px 0;
+}
+
+button {
+    margin-top: 20px;
+}
+
+</style>
